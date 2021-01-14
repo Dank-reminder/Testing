47cffdb915cbb758a4567cdf584c05f90a29044a
SykoticCataclysm <krjf1802@gmail.com>
2021-01-14 15:59:19 +0000

    items added

--- EspModule.lua
+++ EspModule.lua
@@ -7,6 +7,7 @@
 
 local Esp = {
 	Container = {},
+	ItemContainer = {},
 	Settings = {
 		Enabled = false,
         Name = false,
@@ -35,6 +36,20 @@ local CheckVis = newcclosure(function(esp, inview)
 	esp.Name.Visible = Esp.Settings.Name
 	esp.Box.Visible = Esp.Settings.Box
 	esp.Health.Visible = Esp.Settings.Health
+	esp.Distance.Visible = Esp.Settings.Distance
+	esp.Tracer.Visible = Esp.Settings.Tracer
+end)
+
+local CheckItemVis = newcclosure(function(esp, inview)
+	if not inview or (esp.Item.Position - workspace.CurrentCamera.CFrame.Position).Magnitude > Esp.Settings.Range then
+		esp.Name.Visible = false
+		esp.Box.Visible = false
+		esp.Distance.Visible = false
+		esp.Tracer.Visible = false
+		return
+	end
+	esp.Name.Visible = Esp.Settings.Name
+	esp.Box.Visible = Esp.Settings.Box
 	esp.Distance.Visible = Esp.Settings.Distance
 	esp.Tracer.Visible = Esp.Settings.Tracer
 end)
@@ -87,8 +102,8 @@ Esp.Add = function(plr, root, col)
 				Holder.Box.Size = BoxSize
 				Holder.Box.Position = Vector2.new(Pos.X - BoxSize.X / 2, Pos.Y - BoxSize.Y / 2)
 				Holder.Health.Color = Health > 0.66 and Color3.new(0, 1, 0) or Health < 0.33 and Color3.new(1, 0, 0) or Color3.new(1, 1, 0)
-				Holder.Health.Size = Vector2.new(3, BoxSize.Y * Health)
-				Holder.Health.Position = Vector2.new(Pos.X - (BoxSize.X / 2 + 4), (Pos.Y - BoxSize.Y / 2) + ((1 - Health) * BoxSize.Y))
+				Holder.Health.Size = Vector2.new(BoxSize.X / 8, BoxSize.Y * Health)
+				Holder.Health.Position = Vector2.new(Pos.X - (BoxSize.X / 2 + BoxSize.X / 8 + 4), (Pos.Y - BoxSize.Y / 2) + ((1 - Health) * BoxSize.Y))
 				Holder.Distance.Text = math.floor((root.Position - workspace.CurrentCamera.CFrame.Position).Magnitude) .. " Studs"
 				Holder.Distance.Position = Vector2.new(Pos.X, Pos.Y + BoxSize.X / 2 + 4)
 				Holder.Tracer.To = Vector2.new(Pos.X, Pos.Y + BoxSize.Y / 2)
@@ -98,6 +113,61 @@ Esp.Add = function(plr, root, col)
 			Holder.Name.Visible = false
 			Holder.Box.Visible = false
 			Holder.Health.Visible = false
+			Holder.Distance.Visible = false
+			Holder.Tracer.Visible = false
+		end
+	end)
+end
+
+Esp.AddItem = function(name, item, col)
+	if Esp.ItemContainer[item] then
+		for i, v in next, Esp.ItemContainer[item] do
+			v:Remove()
+		end
+		Esp.ItemContainer[item] = nil
+	end
+	local Holder = {
+		Name = Drawing.new("Text"),
+		Box = Drawing.new("Square"),
+		Distance = Drawing.new("Text"),
+		Tracer = Drawing.new("Line"),
+		Item = item,
+		YMult = item.Size.Y / item.Size.X,
+		Colour = col
+	}
+	Esp.ItemContainer[item] = Holder
+    Holder.Name.Text = name
+    Holder.Name.Size = Esp.Settings.TextSize
+    Holder.Name.Center = true
+	Holder.Name.Color = col
+    Holder.Name.Outline = true
+    Holder.Box.Thickness = 1
+	Holder.Box.Color = col
+	Holder.Box.Filled = false
+    Holder.Distance.Size = Esp.Settings.TextSize
+    Holder.Distance.Center = true
+	Holder.Distance.Color = col
+	Holder.Distance.Outline = true
+	Holder.Tracer.From = TracerStart
+	Holder.Tracer.Color = col
+    Holder.Tracer.Thickness = 1
+	Holder.Connection = game:GetService("RunService").Stepped:Connect(function()
+		if Esp.Settings.Enabled then
+			local Pos, Vis = workspace.CurrentCamera:WorldToViewportPoint(item.Position)
+			if Vis then
+				local X = 2200 / Pos.Z
+				local BoxSize = Vector2.new(X, X * Holder.YMult)
+				Holder.Name.Position = Vector2.new(Pos.X, Pos.Y - BoxSize.X / 2 - (4 + Esp.Settings.TextSize))
+				Holder.Box.Size = BoxSize
+				Holder.Box.Position = Vector2.new(Pos.X - BoxSize.X / 2, Pos.Y - BoxSize.Y / 2)
+				Holder.Distance.Text = math.floor((item.Position - workspace.CurrentCamera.CFrame.Position).Magnitude) .. " Studs"
+				Holder.Distance.Position = Vector2.new(Pos.X, Pos.Y + BoxSize.X / 2 + 4)
+				Holder.Tracer.To = Vector2.new(Pos.X, Pos.Y + BoxSize.Y / 2)
+			end
+			CheckItemVis(Holder, Vis)
+		elseif Holder.Name.Visible == true then
+			Holder.Name.Visible = false
+			Holder.Box.Visible = false
 			Holder.Distance.Visible = false
 			Holder.Tracer.Visible = false
 		end
@@ -116,6 +186,19 @@ Esp.Remove = newcclosure(function(root)
 		end
 	end
 	Esp.Container[root] = nil
+end)
+
+Esp.RemoveItem = newcclosure(function(item)
+	for i, v in next, Esp.ItemContainer do
+		if i == item then
+			v.Connection:Disconnect()
+			v.Name:Remove()
+			v.Box:Remove()
+			v.Distance:Remove()
+			v.Tracer:Remove()
+		end
+	end
+	Esp.ItemContainer[item] = nil
 end)
 
 Esp.TeamCheck = newcclosure(function(plr)
@@ -154,9 +237,21 @@ Esp.ToggleRainbow = newcclosure(function(bool)
 				v.Distance.Color = Colour
 				v.Tracer.Color = Colour
 			end
+			for i, v in next, Esp.ItemContainer do
+				v.Name.Color = Colour
+				v.Box.Color = Colour
+				v.Distance.Color = Colour
+				v.Tracer.Color = Colour
+			end
 		end)
 	else
 		for i, v in next, Esp.Container do
+			v.Name.Color = v.Colour
+			v.Box.Color = v.Colour
+			v.Distance.Color = v.Colour
+			v.Tracer.Color = v.Colour
+		end
+		for i, v in next, Esp.ItemContainer do
 			v.Name.Color = v.Colour
 			v.Box.Color = v.Colour
 			v.Distance.Color = v.Colour
